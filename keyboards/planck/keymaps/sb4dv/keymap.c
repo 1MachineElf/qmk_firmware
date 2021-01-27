5004a77295c0a66580911d0276f89addd8e904ea
1MachineElf <1MachineElf@gmail.com>
2021-01-27 00:28:10 -0500

    fixed sb4dv planck keymap

--- keyboards/planck/keymaps/sb4dv/keymap.c
+++ keyboards/planck/keymaps/sb4dv/keymap.c
@@ -47,10 +47,10 @@ enum layers {
     K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, \
     K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A  \
   ) \
-  LAYOUT_ortho_4x12_wrapper( \
+  LAYOUT_ortho_4x12( \
     KC_ESC,  K01,    K02,     K03,      K04,     K05,     K06,     K07,     K08,     K09,     K0A,     KC_BSPC, \
     TEHW,    K11,    K12,     K13,      K14,     K15,     K16,     K17,     K18,     K19,     K1A,     KC_ENT, \
-    KC_MLSF, K21,    K22,     K23,      K24,     K25,     K26,     K27,     K28,     K29,     K2A,     TEHS,  \
+    KC_LSFT, K21,    K22,     K23,      K24,     K25,     K26,     K27,     K28,     K29,     K2A,     TEHS,  \
     KC_LCTL, KC_LGUI, KC_LALT, MODS, _______,  KC_LSFT,  _______,  KC_SPC,  NUMS,    KC_RALT, KC_RGUI, KC_RCTL  \
   )
 #define LAYOUT_ortho_4x12_base_wrapper(...)       LAYOUT_ortho_4x12_base(__VA_ARGS__)
