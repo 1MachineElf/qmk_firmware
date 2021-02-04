4516b06fd173e123f40ccc7b75f835c1b092e9f4
1MachineElf <1MachineElf@gmail.com>
2021-02-04 15:20:07 -0500

    fixed sb4dv planck build error

--- keyboards/planck/keymaps/sb4dv/keymap.c
+++ keyboards/planck/keymaps/sb4dv/keymap.c
@@ -36,6 +36,8 @@ enum layers {
 
 #define TEHW LT(_WASD, KC_ESC)
 
+#define TEHM LT(_WASD, KC_MINS)
+
 // Based on the ortho_4x12 layout from drashna
 
 // Use an expanded macro with VA_ARGS to ensure that the common
@@ -49,7 +51,7 @@ enum layers {
   ) \
   LAYOUT_ortho_4x12( \
     KC_ESC,  K01,    K02,     K03,      K04,     K05,     K06,     K07,     K08,     K09,     K0A,     KC_BSPC, \
-    TEHW,    K11,    K12,     K13,      K14,     K15,     K16,     K17,     K18,     K19,     K1A,     KC_ENT, \
+    TEHW,    K11,    K12,     K13,      K14,     K15,     K16,     K17,     K18,     K19,     K1A,     TEHM, \
     KC_LSFT, K21,    K22,     K23,      K24,     K25,     K26,     K27,     K28,     K29,     K2A,     TEHS,  \
     KC_LCTL, KC_LGUI, KC_LALT, MODS, _______,  KC_LSFT,  _______,  KC_SPC,  NUMS,    KC_RALT, KC_RGUI, KC_RCTL  \
   )
@@ -85,5 +87,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
   return update_tri_layer_state(state, _MODS1, _NUMS, _MODS2);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (keycode == KC_QUOT && record->event.pressed) {
+        bool rc = true;
+        uint8_t mods = 0;
+        if ((mods = get_oneshot_mods()) && !has_oneshot_mods_timed_out()) {
+            clear_oneshot_mods();
+            unregister_mods(mods);
+            rc = false;
+        }
+        if ((mods = get_oneshot_locked_mods())) {
+            clear_oneshot_locked_mods();
+            unregister_mods(mods);
+            rc = false;
+        }
+        if (is_oneshot_layer_active()) {
+            layer_clear();
+            rc = false;
+        }
+        return rc;
+    }
+    return true;
 }
 
